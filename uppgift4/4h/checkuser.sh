c8d6fbc819c5e139d2b8ead3068ae033613da47a
inuinus <linusfrisk01@gmail.com>
2025-02-27 08:55:31 +0100

    finished 4h

--- uppgift4/4h/checkuser.sh
+++ uppgift4/4h/checkuser.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
 logged_in_users=$(who | cut -d' ' -f1)
-
 #ifall man inte ger en input direkt
 if [ -z "$1" ]; then
 	echo "skriv ett användarnamn:"
 	read user_input
 
-	if [ "$logged_in_users" == "$user_input" ]; then
-		echo "inloggad"
+	if [[ $(echo "$logged_in_users" | grep "^$user_input$") ]]; then
 		exit 0
 	else
-		echo "användaren är inte inloggad"
 		exit 1
 	fi
 else #om inputen ges direkt
-	if [ "$logged_in_users" == "$1" ]; then
-                echo "inloggad"
+	if [[ $(echo "$logged_in_users" | grep "^$1$") ]]; then
                 exit 0
         else
-                echo "användaren är inte inloggad"
-                exit 1
+		exit 1
 	fi
 fi
